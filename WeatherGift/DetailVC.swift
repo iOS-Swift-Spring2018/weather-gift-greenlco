dd4bcf789163e37b86fadabd77e7384a0f6b6c4f
Mom <cgreenleaf14@connorgreenleaf.bc.edu>
2018-03-19 00:04:50 -0400

    Initial Commit

---
+++ WeatherGift/DetailVC.swift
@@ -0,0 +1,28 @@
+//
+//  DetailVC.swift
+//  WeatherGift
+//
+//  Created by Connor Greenleaf on 3/18/18.
+//  Copyright © 2018 Connor Greenleaf. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var summaryLabel: UILabel!
+    @IBOutlet weak var currentImage: UIImageView!
+    
+    var currentPage = 0
+    var locationsArray = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        locationLabel.text = locationsArray[currentPage]
+    }
+
+}
